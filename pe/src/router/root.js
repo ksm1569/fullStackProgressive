5af7ebfd6c341620389b4e52ac6bf7b3a1a1e239
ksm1569 <tnals1564@naver.com>
2024-04-16 14:34:58 +0900

    outlet

--- pe/src/router/root.js
+++ pe/src/router/root.js
@@ -1,19 +1,47 @@
-import {Suspense, lazy} from "react";
-const {createBrowserRouter} = require("react-router-dom");
+import { Suspense, lazy } from "react";
+const { createBrowserRouter } = require("react-router-dom");
 
-const Loding = <div className={'bg-red-700'}>Loading....</div>
-const Main = lazy(() => import("../pages/MainPage"))
-const About = lazy(() => import("../pages/AboutPage"))
+const Loding = <div className={"bg-red-700"}>Loading....</div>;
+const Main = lazy(() => import("../pages/MainPage"));
+const About = lazy(() => import("../pages/AboutPage"));
+const TodoIndex = lazy(() => import("../pages/todo/indexPage"));
+const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
 const root = createBrowserRouter([
-    {
-        path: '',
-        element: <Suspense fallback={Loding}><Main/></Suspense>
-    },
-    {
-        path: 'about',
-        element: <Suspense fallback={Loding}><About/></Suspense>
-    }
-])
+  {
+    path: "",
+    element: (
+      <Suspense fallback={Loding}>
+        <Main />
+      </Suspense>
+    ),
+  },
+  {
+    path: "about",
+    element: (
+      <Suspense fallback={Loding}>
+        <About />
+      </Suspense>
+    ),
+  },
+  {
+    path: "todo",
+    element: (
+      <Suspense fallback={Loding}>
+        <TodoIndex />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "list",
+        element: (
+          <Suspense fallback={Loding}>
+            <TodoList />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
-export default root
+export default root;
